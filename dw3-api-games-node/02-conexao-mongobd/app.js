cc16cb1b31c44b3b5a8b7b8c9ef1b1c8951f75ba
DSM3 <fatec-dsm3@academico.intra>
2025-08-13 22:53:14 -0300

    LucasBParada

---
+++ dw3-api-games-node/02-conexao-mongobd/app.js
@@ -0,0 +1,33 @@
+import express from "express"
+import mongoose from "mongoose"
+const app = express()
+import Game from "./models/Games.js"
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+//Iniciando a conexão com o banco de dados do MongoBD
+mongoose.connect("mongodb://127.0.0.1:27017/api-thegames")
+
+
+
+app.get("/", async (req,res) => {
+    try{
+        const games = await Game.find()
+        res.status(200).json({games : games}) //Código 200: OK (Requisição bem sucedida.)
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error:'Erro interno do servidor.'})
+    }
+    
+})
+
+//Rodando API na porta 4000
+const port = 4000
+app.listen(port,(error) => {
+    if(error){
+        console.log(error)
+    }
+    console.log(`API rodando em http://localhost:${port}`)
+})
